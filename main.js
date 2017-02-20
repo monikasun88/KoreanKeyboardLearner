9734e6aa4672a2e51fdcf0de1db5ece193d66c0b
monikasun88 <monikasun88@gmail.com>
2017-02-20 00:23:08 -0800

    Update
    Fixed bug with shift and updated website layout

--- main.js
+++ main.js
@@ -28,6 +28,23 @@ $(document).ready(function() {
       122, 120, 99, 118, 98, 110, 109]
     };
 
+  var colorScheme = {
+    yellow: "rgba(250, 223, 99, 1)",
+    red: "rgba(240, 96, 96, 1)",
+    brown: "rgba(57, 58, 16, 1)",
+    blue: "rgba(71, 86, 87, 1)",
+    white: "rgba(198, 197, 185, 0.7)"
+  }
+
+  $("#instruction").html(function() {
+    var genInstruct = "Type the equivalent character to increase your streak<br>"
+    var studyInstruct = "<span style='color:"+colorScheme.red+"'>SPACE</span>" +
+      "<span style='color:"+colorScheme.white+"'> to go to the key</span><br>"
+    var nextInstruct = "<span style='color:"+colorScheme.yellow+"'>&rarr;</span>" +
+      "<span style='color:"+colorScheme.white+"'> to skip to the next character</span><br>"
+    return(genInstruct+studyInstruct+nextInstruct)
+  })
+
   // Calculate a random korean character from array without repeat
   function NextKoreanChar(currQuestionText) {
     var randChar = koreanKey.koreanCharacter[
@@ -53,6 +70,15 @@ $(document).ready(function() {
     return(isRight)
   };
 
+  // Go to next character and reset
+  function NextButton() {
+    $("#question-text").html(NextKoreanChar($("#question-text").text()));
+    $("#numCorrect")
+      .stop(true, true)
+      .html(0)
+      .fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
+  }
+
   // Add Korean characters to keyboard keyboard
   $.each(koreanKey.koreanCharacter, function(index, value) {
     var valueClass = (koreanKey.shiftBoolean[index] ?
@@ -76,24 +102,26 @@ $(document).ready(function() {
   });
 
   // Next button goes to next letter
-  $("#next-button").click(function() {
-    $("#question-text").html(NextKoreanChar($("#question-text").text()));
-    $("#numCorrect")
-      .stop(true, true)
-      .html(0)
-      .fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
-  });
+  $("#next-button").click(function() {NextButton()});
 
   $(document).on("keydown", function (e) {
-    {
+    console.log(e)
+    // Escape pressed
+    if (e.which === 27) {
 
-      // Escape pressed
-      if (e.which === 27) {
+      $("#popup1").attr("class", "overlay");
 
-        $("#popup1").attr("class", "overlay");
+    // Right button goes to the next character//
+  } else if (e.originalEvent.key === "ArrowRight") {
+    NextButton();
 
-      // Letter key pressed (if not letter key do nothing)
-    } else if (koreanKey.characterCode.indexOf(e.which)+32 >= 0) {
+    // Tab pressed opens study menu
+  } else if (e.keyCode === "0" | e.keyCode === 32) {
+      var currShown = ($("#popup1").attr("class") === "overlay" ?
+        "overlayShown" : "overlay")
+      $("#popup1").attr("class", currShown);
+    // Letter key pressed (if not letter key do nothing)
+    } else if (koreanKey.characterCode.indexOf(e.which+32) >= 0) {
 
         // Calculate key pressed
         var keyVal = e.which + (e.shiftKey ? 0 : 32)
@@ -120,10 +148,12 @@ $(document).ready(function() {
             .stop(true, true)
             .effect("highlight", {color: "rgba(102, 16, 31, 1)"}, 200)
 
-          $("#numCorrect").html(0).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
+          $("#numCorrect")
+            .stop(true, true)
+            .html(0)
+            .fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
 
         }
       }
-    }
   });
 });
