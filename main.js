65f537e83bf02ea87d90236ec9a092efd31a4a9d
monikasun88 <monikasun88@gmail.com>
2017-02-19 17:21:26 -0800

    Update
    Update logic, looks, rough draft 1

--- main.js
+++ main.js
@@ -11,20 +11,24 @@ $(document).ready(function() {
     keyboardCharacter:
     ["q", "q", "w", "w", "e", "e", "r", "r", "t", "t",
       "y", "u", "i", "o", "o", "p", "p",
-      "a", "s", "d", "f", "g", "h", "j", "k", "l",
+      "a", "s", "d", "f", "g",
+      "h", "j", "k", "l",
       "z", "x", "c", "v", "b", "n", "m"],
     shiftBoolean:
     [0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
       0, 0, 0, 0, 1, 0, 1,
-      0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0,
+      0, 0, 0, 0,
       0, 0, 0, 0, 0, 0, 0],
     characterCode:
     [113, 81, 119, 87, 101, 69, 114, 82, 116, 84,
       121, 117, 105, 111, 79, 112, 80,
-      97, 115, 100, 102, 103, 104, 106, 107, 108,
+      97, 115, 100, 102, 103,
+      104, 106, 107, 108,
       122, 120, 99, 118, 98, 110, 109]
     };
 
+  // Calculate a random korean character from array without repeat
   function NextKoreanChar(currQuestionText) {
     var randChar = koreanKey.koreanCharacter[
       Math.floor(Math.random() * koreanKey.koreanCharacter.length)];
@@ -36,70 +40,75 @@ $(document).ready(function() {
     }
   };
 
+  // Check answer on website against typed
   function CheckAnswer(ans) {
-    if (koreanKey.characterCode.indexOf(ans) >= 0) {
-      if ($("#question-text").text() === String.fromCharCode(ans + 12497)) {
-        var isRight = 1;
-      } else {
-        var isRight = 0;
-      }
+    var isRight;
+    if ($("#question-text").text() ===
+      koreanKey.koreanCharacter[koreanKey.characterCode.indexOf(ans)]) {
+      isRight = 1;
+    } else {
+      isRight = 0;
     }
     // console.log(isRight);
     return(isRight)
   };
 
-  //
-  // $("#temporary-next-button").click(function () {
-  //   {
-  //     // // alert("temporary-next-button-onclick");
-  //     // // alert(koreanChar[2]);
-  //     // var randKoreanChar = GenRandKoreanChar(koreanChar);
-  //     // while ($("#question-text").text() == koreanChar[randKoreanChar]) {
-  //     //   // console.log("repeated");
-  //     //   var randKoreanChar = GenRandKoreanChar(koreanChar);
-  //     // }
-  //     // // console.log(koreanChar[randKoreanChar])
-  //     // $("#question-text").html(koreanChar[randKoreanChar]);
-  //     //////
-  //     $("#question-text").html(koreanChar[NextKoreanChar()])
-  //   }
-  // });
+  // Add Korean characters to keyboard keyboard
+  $.each(koreanKey.koreanCharacter, function(index, value) {
+    var valueClass = (koreanKey.shiftBoolean[index] ?
+      "koreanChar shiftChar" : "koreanChar");
+    $("#"+koreanKey.keyboardCharacter[index]+"-key").append(
+      "<div class='"+valueClass+"'>"+value+"</div>"
+    )
+  });
 
-  $(document).on("keypress", function (e) {
+  // Open/close keyboard key on click study/click close or hit escape
+  $("#study").click(function() {
+    $("#popup1").attr("class", "overlayShow");
+  });
+  $(".close").click(function() {
+    $("#popup1").attr("class", "overlay");
+  });
+
+  $(document).on("keydown", function (e) {
     {
-      // Correct and incorrect answer responses
-      if (CheckAnswer(e.which) === 1) {
-        $("#question-area").stop(true, true).effect("highlight", {color: "#008800"})
-        $("#question-text").html(NextKoreanChar($("#question-text").text()));
-      } else {
-        $("#question-area").stop(true, true).effect("highlight", {color: "#CC0000"})
+      // Escape pressed
+      if (e.which === 27) {
+
+        $("#popup1").attr("class", "overlay");
+
+      // Letter key pressed (if not letter key do nothing)
+      } else if (koreanKey.characterCode.indexOf(e.which) >= 0) {
+
+        // Calculate key pressed
+        var keyVal = e.which + (e.shiftKey ? 0 : 32)
+
+        // If answer matches key, flash green, calculate streak and display next character
+        if (CheckAnswer(keyVal) === 1) {
+
+          $("#question-area")
+            .stop(true, true)
+            .effect("highlight", {color: "rgba(16, 155, 58, 1)"})
+
+          $("#numCorrect").html(function() {
+            var numCorr = parseInt($("#numCorrect").text()) + 1
+            $("#numCorrect").css("font-size", String(14+numCorr/40*30)+"px");
+            return numCorr
+          })
+
+          $("#question-text").html(NextKoreanChar($("#question-text").text()));
+
+        // If answer does not match key, flash red, change streak to zero and flash x2
+        } else {
+
+          $("#question-area")
+            .stop(true, true)
+            .effect("highlight", {color: "rgba(102, 16, 31, 1)"}, 200)
+
+          $("#numCorrect").html(0).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
+
+        }
       }
     }
   });
-  $(document).on("keyup", function (e) {
-      $("#"+String.fromCharCode(e.which).toLowerCase()+"-key").attr("class", "keys off");
-  });
-});
-
-
-var onkeydown = (function (ev) {
-  var key;
-  var isShift;
-  if (window.event) {
-    key = window.event.keyCode;
-    isShift = !!window.event.shiftKey; // typecast to boolean
-  } else {
-    key = ev.which;
-    isShift = !!ev.shiftKey;
-  }
-  if ( isShift ) {
-    switch (key) {
-      case 16: // ignore shift key
-        break;
-      default:
-        alert(key);
-        // do stuff here?
-        break;
-    }
-  }
 });
